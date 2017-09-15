ee4c61271a73961881f426d041bf473df20fa5ec
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2017-09-15 10:24:29 -0500

    Initial commit

---
+++ FCMClient/FCMClient/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
+namespace FCMClient
+{
+    [Activity(Label = "FCMClient", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView msgText;
+        const string TAG = "MainActivity";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            Log.Debug(TAG, "google app id: " + Resource.String.google_app_id);
+
+            SetContentView(Resource.Layout.Main);
+
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+            if(Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+
+            IsPlayServicesAvailable();
+            var logTokenButton = FindViewById<Button>(Resource.Id.logTokenButton);
+            var subscribebutton = FindViewById<Button>(Resource.Id.subscribeButton);
+            logTokenButton.Click += delegate
+            {
+                Log.Debug(TAG, "Instance ID Token: " + FirebaseInstanceId.Instance.Token);
+            };
+
+            subscribebutton.Click += delegate
+            {
+                FirebaseMessaging.Instance.SubscribeToTopic("news");
+                Log.Debug(TAG, "Suscrito a las notificaciones remotas");
+            };
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if(resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+                else
+                {
+                    msgText.Text = "This Device is not suported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+    }
+}
+
---
+++ FCMClient/FCMClient/MyFirebaseMessagingService.cs
@@ -0,0 +1,40 @@
+﻿
+using Android.App;
+using Android.Content;
+using Firebase.Messaging;
+using Android.Util;
+using FCMClient;
+
+namespace FCMClient
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body" + message.GetNotification().Body);
+            SendNotification(message);
+        }
+
+        void SendNotification(RemoteMessage messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Drawable.common_full_open_on_phone)
+                .SetContentTitle(messageBody.GetNotification().Title)
+                .SetContentText(messageBody.GetNotification().Body)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
